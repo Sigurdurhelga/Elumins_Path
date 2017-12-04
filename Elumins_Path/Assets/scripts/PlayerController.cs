c83b7184712642fdbd044da81b0721edc2e40bfa
Siggi <siggnja@gmail.com>
2017-12-04 21:36:56 +0000

    Changed some OnTriggerEvent

--- Elumins_Path/Assets/scripts/PlayerController.cs
+++ Elumins_Path/Assets/scripts/PlayerController.cs
@@ -7,12 +7,19 @@ public class PlayerController : MonoBehaviour
 
     public float speed;             //Floating point variable to store the player's movement speed.
 
+    public GameObject RedGem;
+
+    private Vector3 PortalPosition;
     private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
     // Use this for initialization
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        PortalPosition.x = 2.0f;
+        PortalPosition.y = 2.0f;
+        PortalPosition.z = 0f;
+
     }
 
     // Update is called once per frame
@@ -29,7 +36,16 @@ public class PlayerController : MonoBehaviour
     {
         if (col.gameObject.tag == "BlueGem")
         {
-            Destroy(col.gameObject);
+            GameObject red = GameObject.FindGameObjectWithTag("RedGem");
+            if (red != null)
+            {
+                Destroy(red);
+            }
+            else
+            {
+                red = (GameObject)Instantiate(Resources.Load("Gem"), PortalPosition, Quaternion.identity);
+                red.SetActive(true);
+            }
         }
     }
 }
