950a2de615760f3d11861e12fc96b876bf88d870
Siggi <siggnja@gmail.com>
2017-12-07 17:35:06 +0000

    Added portal disabled sprite and transition when light is enabled

--- Elumins_Path/Assets/scripts/levelmanager.cs
+++ Elumins_Path/Assets/scripts/levelmanager.cs
@@ -2,36 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class levelmanager : MonoBehaviour {
-	public int a;
+public class levelmanager : MonoBehaviour
+{
+    public int a;
 
-	private List<GameObject> gems = new List<GameObject>();
-	private ParticleSystem portal;
-	private Light roomLight;
+    private List<GameObject> gems = new List<GameObject>();
+    private ParticleSystem portal;
+    private Light roomLight;
 
-	int requiredGems;
+    int requiredGems;
 
-	// Use this for initialization
-	void Start () {
-		portal = GetComponentInChildren<ParticleSystem>();
-		roomLight = GetComponentInChildren<Light>();
-		
-		var emission = portal.emission;
-		emission.enabled = false;
+    // Use this for initialization
+    void Start()
+    {
+        portal = GetComponentInChildren<ParticleSystem>();
+        roomLight = GetComponentInChildren<Light>();
 
-		foreach(GameObject gem in GameObject.FindGameObjectsWithTag("levelGem")){
-			gems.Add(gem);
-		}
-		requiredGems = gems.Count;
-	}
+        portal.Stop();
+        foreach (GameObject gem in GameObject.FindGameObjectsWithTag("levelGem"))
+        {
+            gems.Add(gem);
+        }
+        requiredGems = gems.Count;
+    }
 
-	public void gemActivated(){
-		requiredGems -= 1;
-		if(requiredGems <= 0){
-			roomLight.enabled = true;
-			var emission = portal.emission;
-			emission.enabled = true;
-		}
-	}
-	
+    public void gemActivated()
+    {
+        requiredGems -= 1;
+        if (requiredGems <= 0)
+        {
+            roomLight.enabled = true;
+            portal.Play();
+            GameObject dis_port = GameObject.FindGameObjectWithTag("Disabled_Portal");
+            dis_port.SetActive(false);
+        }
+    }
+
 }
