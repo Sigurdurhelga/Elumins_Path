734ec76eb23e63f8d565e28a43ee5494714d7108
sabbisun <sabbisun@gmail.com>
2017-12-07 13:45:00 +0000

    Added TextMeshPro and used it in menu

---
+++ Elumins_Path/Assets/scripts/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public void ContinueButton(string NewGameLevel)
+    {
+        SceneManager.LoadScene(NewGameLevel);
+    }
+
+    public void NewGameButton(string NewGameLevel)
+    {
+        SceneManager.LoadScene(NewGameLevel);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
