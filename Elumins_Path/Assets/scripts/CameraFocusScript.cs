68b3a1ff2a3031c0a69ddf6fdf6968d0af95f36a
siggihelga <sigurdurhel15@ru.is>
2017-12-10 15:04:54 +0000

    fixed camerafocus to work with new camerascript, also made movement more snappy

--- Elumins_Path/Assets/scripts/CameraFocusScript.cs
+++ Elumins_Path/Assets/scripts/CameraFocusScript.cs
@@ -1,43 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using CameraSmooth2D;
 
 public class CameraFocusScript : MonoBehaviour {
 
     private Transform camera_transform;
-    private CameraManager camera_script;
+    private DeadZoneCamera camera_script;
+    private Transform playerRef;
 
     private bool playerIn = false;
 
 	// Use this for initialization
 	void Start () {
         camera_transform = Camera.main.transform;
-        camera_script = camera_transform.GetComponent<CameraManager>();
-
+        camera_script = camera_transform.GetComponent<DeadZoneCamera>();
 	}
 
-    IEnumerator smoothMoveCamera()
+    IEnumerator smoothMoveCameraToPos()
     {
+        Debug.Log("entered smooth to pos");
         Vector3 source = camera_transform.position;
         float pos = 0;
-        while (pos < 1)
+        while (pos < 1 && playerIn)
         {
             pos += 0.01f;
             camera_transform.position = Vector3.Lerp(source, transform.position, pos);
             yield return new WaitForSeconds(0.01f);
         }
     }
+    IEnumerator smoothMoveCameraToPlayer()
+    {
+        Vector3 source = camera_transform.position;
+        float pos = 0;
+        Debug.Log("entered smooth to player" + pos);
+        while (pos < 1)
+        {
+            pos += 0.01f;
+            Debug.Log("loop smooth to player" + pos);
+            Vector3 desiredPos = new Vector3(playerRef.position.x, playerRef.position.y, -10);
+            camera_transform.position = Vector3.Lerp(source, desiredPos , pos);
+            yield return new WaitForSeconds(0.01f);
+        }
+        camera_script.enabled = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("hey");
         if(collision.transform.tag == "Player")
         {
-            Debug.Log("Player entered");
+            if(!playerRef)
+                playerRef = collision.transform;
+            Debug.Log("plaer entered");
             playerIn = true;
             camera_script.enabled = false;
-            StartCoroutine(smoothMoveCamera());
+            StartCoroutine(smoothMoveCameraToPos());
         }
     }
 
@@ -45,9 +61,10 @@ public class CameraFocusScript : MonoBehaviour {
     {
         if(collision.transform.tag == "Player")
         {
-            Debug.Log("Player exited");
+            Debug.Log("plaer exit");
             playerIn = false;
-            camera_script.enabled = true;
+            StopCoroutine(smoothMoveCameraToPos());
+            StartCoroutine(smoothMoveCameraToPlayer());
         }
     }
 
