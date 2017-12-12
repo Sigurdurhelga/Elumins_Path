ccb445000b906612114f9a3ca5932deb8a8ea7fe
siggihelga <sigurdurhel15@ru.is>
2017-12-12 16:34:45 +0000

    dialogue manager done

---
+++ Elumins_Path/Assets/scripts/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public Text dialogueText;
+	public Animator animator;
+	private Queue<string> sentences = new Queue<string>();
+	private bool inConverstation = false;
+
+	private void Update(){
+
+		if(inConverstation && Input.GetKeyDown(KeyCode.Space)){
+			displayeNextSentence();
+		}
+
+	}
+
+	public void StartDialogue(Dialogue dialogue){
+
+		sentences.Clear();
+
+		animator.SetBool("isOpen", true);
+
+		foreach(string sentence in dialogue.sentences){
+			sentences.Enqueue(sentence);
+		}
+
+		inConverstation = true;
+		displayeNextSentence();
+	}
+
+	IEnumerator typeText(string text){
+		foreach(char c in text){
+			dialogueText.text += c;
+			yield return null;
+		}
+	}
+
+	public void displayeNextSentence(){
+		if(sentences.Count == 0){
+			endDialogue();
+		}
+		else{
+			string sentence = sentences.Dequeue();
+			dialogueText.text = "";
+			StopAllCoroutines();
+			StartCoroutine(typeText(sentence));
+		}
+	}
+
+	public void endDialogue(){
+		animator.SetBool("isOpen", false);
+	}
+
+}
---
+++ Elumins_Path/Assets/scripts/DialogueTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+	public Dialogue dialogue;
+
+	private DialogueManager dialogueManager;
+	private bool doneTriggering = false;
+
+	private void Start(){
+		dialogueManager = GameObject.FindGameObjectWithTag("dialogueManager").GetComponent<DialogueManager>();
+	}
+
+	public void TriggerDialogue(){
+		if(!doneTriggering)
+			dialogueManager.StartDialogue(dialogue);
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider){
+		if(collider.tag == "Player"){
+			TriggerDialogue();
+			doneTriggering = true;
+		}
+	}
+	
+}
