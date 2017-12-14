6cae291fbc8b16a0f5a0604c013558ce342647f1
siggihelga <sigurdurhel15@ru.is>
2017-12-14 21:22:40 +0000

    setup resolution and graphics dropdowns

--- Elumins_Path/Assets/scripts/SettingsMenuScript.cs
+++ Elumins_Path/Assets/scripts/SettingsMenuScript.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
 
 public class SettingsMenuScript : MonoBehaviour {
 
     public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+    public TMP_Dropdown graphicsDropdown;
+    
+    private Resolution[] resolutions;
+
+    private void Start(){
+
+        /*SETTING UP RESOLUTIONS*/
+        resolutions = Screen.resolutions;
+        List<string> options = new List<string>();
+        int resolutionSelected = 0;
+        for(int i = 0; i < resolutions.Length; i++){
+            options.Add(resolutions[i].width+"x"+resolutions[i].height);
+            resolutionDropdown.onValueChanged.AddListener(
+                delegate { 
+                    Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, true);
+                        });
+            if(Screen.currentResolution.height == resolutions[i].height && Screen.currentResolution.width == resolutions[i].width)
+                resolutionSelected = i;
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = resolutionSelected;
+        /*SETTING UP GRAPHICS*/
+        options.Clear();
+        foreach(string quality in QualitySettings.names){
+            options.Add(quality);
+        }
+        graphicsDropdown.AddOptions(options);
+        graphicsDropdown.value = QualitySettings.GetQualityLevel();
+
+    }
 
     public void SetVolume(float volume)
     {
@@ -26,8 +60,9 @@ public class SettingsMenuScript : MonoBehaviour {
         // Does nothing currently
     }
 
-    private void SetQuality(int qualityIndex)
+    public void SetQuality(int qualityIndex)
     {
+        Debug.Log(qualityIndex);
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 }
