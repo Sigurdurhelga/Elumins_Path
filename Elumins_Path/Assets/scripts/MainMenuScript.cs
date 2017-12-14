e1abfd3e5a706b5b6f29e7defa792ed13b934023
Sigurður Gunnar Njásslon <Siggnja@gmail.com>
2017-12-14 15:05:08 +0000

    Again fixed bug when continue starts at first level

--- Elumins_Path/Assets/scripts/MainMenuScript.cs
+++ Elumins_Path/Assets/scripts/MainMenuScript.cs
@@ -18,7 +18,6 @@ public class MainMenuScript : MonoBehaviour
 
     public void NewGameButton(string NewGameLevel)
     {
-        string temp = "CurrentLevel";
         PlayerPrefs.DeleteAll();
         controller.LoadNextLevel(1.ToString());
     }
