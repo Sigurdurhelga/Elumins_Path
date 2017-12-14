57f95c626a91a1262af51cb6262fee4fe2561206
Sigurður Gunnar Njásslon <Siggnja@gmail.com>
2017-12-14 19:42:48 +0000

    TwinCrystal prefab seems to work fine

--- Elumins_Path/Assets/scripts/Gems/TwinCrystal.cs
+++ Elumins_Path/Assets/scripts/Gems/TwinCrystal.cs
@@ -5,13 +5,17 @@ using System.Collections;
 public class TwinCrystal : GemParent
 {
     private GameObject Brother;
+    private GameObject MyWall;
     public bool Active;
     public override void Start()
     {
         base.Start();
         Active = false;
+        if (tag == "Brother1") Brother = transform.parent.parent.GetChild(1).GetChild(2).gameObject;
+        else Brother = transform.parent.parent.GetChild(1).GetChild(2).gameObject;
         if (tag == "Brother1") Brother = GameObject.FindGameObjectWithTag("Brother2");
         else Brother = GameObject.FindGameObjectWithTag("Brother1");
+        MyWall = transform.parent.GetChild(5).gameObject;
         Debug.Log(base.GemLight);
     }
     public override void PowerUp()
@@ -22,11 +26,13 @@ public class TwinCrystal : GemParent
         }
         Active = true;
         base.PowerUp();
+        StartCoroutine(FadeOutWallColor(0.2f));
     }
     public void ShutDown()
     {
         Active = false;
         StartCoroutine(FadeOut(GemLight, 0));
+        StartCoroutine(FadeInWallColor(1f));
         base.Start();
     }
     private IEnumerator FadeOut(Light light, float EndIntensity)
@@ -38,7 +44,30 @@ public class TwinCrystal : GemParent
         }
         dynamicLight.SetActive(false);
         transform.parent.GetChild(3).gameObject.SetActive(false);
-
+    }
+    private IEnumerator FadeOutWallColor(float EndIntensity)
+    {
+        SpriteRenderer WallSprite = MyWall.GetComponent<SpriteRenderer>();
+        Color WallColor = WallSprite.color;
+        while (WallColor.a > EndIntensity)
+        {
+            WallColor.a -= 0.05f;
+            WallSprite.color = WallColor;
+            yield return new WaitForSeconds(0.05f);
+        }
+        MyWall.GetComponent<PolygonCollider2D>().enabled = false;
+    }
+    private IEnumerator FadeInWallColor(float EndIntensity)
+    {
+        MyWall.GetComponent<PolygonCollider2D>().enabled = true;
+        SpriteRenderer WallSprite = MyWall.GetComponent<SpriteRenderer>();
+        Color WallColor = WallSprite.color;
+        while (WallColor.a < EndIntensity)
+        {
+            WallColor.a += 0.05f;
+            WallSprite.color = WallColor;
+            yield return new WaitForSeconds(0.05f);
+        }
 
     }
 
