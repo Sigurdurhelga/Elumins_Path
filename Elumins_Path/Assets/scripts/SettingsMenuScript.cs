fbd23b8c47a3d3fb523aa72933ba27a842b30d67
sabbisun <sabbisun@gmail.com>
2017-12-15 17:55:37 +0000

    Fixed audio proximity in level 3

--- Elumins_Path/Assets/scripts/SettingsMenuScript.cs
+++ Elumins_Path/Assets/scripts/SettingsMenuScript.cs
@@ -11,6 +11,7 @@ public class SettingsMenuScript : MonoBehaviour {
     public AudioMixer audioMixer;
     public TMP_Dropdown resolutionDropdown;
     public TMP_Dropdown graphicsDropdown;
+    public Slider musicSlider;
     
     private Resolution[] resolutions;
 
@@ -47,6 +48,14 @@ public class SettingsMenuScript : MonoBehaviour {
             }
             graphicsDropdown.AddOptions(options);
             graphicsDropdown.value = QualitySettings.GetQualityLevel();
+        }
+        float volumeSetting;
+        if (audioMixer.GetFloat("volume", out volumeSetting))
+        {
+            if (musicSlider)
+            {
+                musicSlider.value = Mathf.InverseLerp(-80, 20, volumeSetting);
+            }
         }
     }
 
