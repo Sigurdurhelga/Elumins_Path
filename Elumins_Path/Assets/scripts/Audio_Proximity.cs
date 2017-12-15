261392f03217f821f50eeadd9989ca6c5ea02125
Sigurður Gunnar Njásslon <Siggnja@gmail.com>
2017-12-15 18:56:54 +0000

    Audio fix

--- Elumins_Path/Assets/scripts/Audio_Proximity.cs
+++ Elumins_Path/Assets/scripts/Audio_Proximity.cs
@@ -2,37 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Audio_Proximity : MonoBehaviour {
+public class Audio_Proximity : MonoBehaviour
+{
 
+
+    public AudioSource _audioPlayer;
     private Transform target;
 
-	public AudioSource audioPlayer;
 
-	
-	void Start () {
+
+    void Start()
+    {
         target = GameObject.FindGameObjectWithTag("Player").transform;
         StartCoroutine(AdjustVolume());
     }
-    
-	IEnumerator AdjustVolume () {
 
-		while(true) {
+    IEnumerator AdjustVolume()
+    {
 
-		    float distanceToTarget = Vector3.Distance(transform.position, target.position); // Assuming that the target is the player or the audio listener
-                
-		    if(distanceToTarget < 1) { distanceToTarget = 1; }
-				
-		    if (distanceToTarget > 10) {
-			    audioPlayer.volume = 0;
-		    } 
-		    else 
-		    {
-			    audioPlayer.volume = 1/Mathf.Pow(distanceToTarget, 2); // this works as a linear function, while the 3D sound works like a logarithmic function, so the effect will be a little different (correct me if I'm wrong)
-		    }
-                
-		    yield return new WaitForSeconds(0.01F); // this will adjust the volume based on distance every 1 second (Obviously, You can reduce this to a lower value if you want more updates per second)
+        while (true)
+        {
 
-		}
+            float distanceToTarget = Vector3.Distance(transform.position, target.position); // Assuming that the target is the player or the audio listener
 
-	}
+            if (distanceToTarget < 1) { distanceToTarget = 1; }
+
+            if (distanceToTarget > 10)
+            {
+                _audioPlayer.volume = 0;
+            }
+            else
+            {
+                _audioPlayer.volume = 1 / Mathf.Pow(distanceToTarget, 2); // this works as a linear function, while the 3D sound works like a logarithmic function, so the effect will be a little different (correct me if I'm wrong)
+            }
+
+            yield return new WaitForSeconds(0.01F); // this will adjust the volume based on distance every 1 second (Obviously, You can reduce this to a lower value if you want more updates per second)
+
+        }
+
+    }
 }
