972b9cb2505b84889b960e78cbb751c42af57d85
siggihelga <sigurdurhel15@ru.is>
2017-12-17 21:26:53 +0000

    endwall fixed

--- Elumins_Path/Assets/scripts/EndGameHandler.cs
+++ Elumins_Path/Assets/scripts/EndGameHandler.cs
@@ -6,6 +6,17 @@ using UnityEngine.SceneManagement;
 public class EndGameHandler : MonoBehaviour {
 
 	public Animator animator;
+	public BoxCollider2D endWall;
+	private bool endwallGone = false;
+
+	private void Update(){
+		if(!endwallGone){
+			if(GameController.instance.GetCurrentLevel() >= 12){
+				endWall.enabled = false;
+				endwallGone = true;
+			}
+		}
+	}
 
 	IEnumerator changeToEndgame(){
 		yield return new WaitForSeconds(7f);
